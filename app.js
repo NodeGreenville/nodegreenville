a1924b6ded0e75068aaa85196763ec44698473f3
Harry Baldwin <harrythree@gmail.com>
2015-11-13 21:05:43 -0500

    added events endpoint and basic events view using meetup api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var hbs = require('express-hbs');
 require('dotenv').config();
 
 var routes = require('./routes/index');
+var events = require('./routes/events');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/events', events);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/events.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var async = require('async');
+var router = express.Router();
+var meetup = require('meetup-api')({
+	key: process.env.MEETUP_API
+});
+
+router.get('/', eventsRoute);
+
+module.exports = router;
+
+function eventsRoute(req, res, next) {
+	async.parallel({ group: getGroup, events: getEvents }, renderIndex.bind(null, res, next));
+}
+
+function getGroup(callback) {
+	meetup.getGroup({urlname: process.env.MEETUP_GROUP_NAME, fields: 'group_photo'}, callback);
+}
+
+function getEvents(callback) {
+	meetup.getEvents({'group_urlname': process.env.MEETUP_GROUP_NAME}, callback);
+}
+
+function renderIndex(res, next, err, results) {
+	if (err) {
+		return next(err);
+	}
+
+	for(var i=0; i<results.events.results.length; i++) {
+		console.log(results.events.results[i]);
+	}
+
+	res.render('events', { group: results.group, events: results.events.results });
+}
