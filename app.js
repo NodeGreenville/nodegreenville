6d2fb731707c8b9ad49df6ddfa0d4e6c56913644
Javis Sullivan <javissullivan@gmail.com>
2015-11-15 22:44:41 -0500

    fixed issue #5037 affectingghostjs as npm module

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('views', __dirname + '/views');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
