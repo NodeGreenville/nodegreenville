42ccccb4072e5877b6889998a1315daa928677a2
Javis Sullivan <javissullivan@gmail.com>
2015-11-19 21:17:42 -0500

    added minor test for blog access

--- test/index.js
+++ test/index.js
@@ -1,8 +1,26 @@
 var request = require('supertest');
 var app = require('../app');
 
-it('should return a 200', function(done){
-	request(app)
-		.get('/')
-		.expect(200, done);
-});
+function checkForMultipleGhostStatusCodes(res) {
+		/* Ghost blog will responde with 303 or 200 */
+		if (200 == res.statusCode || 303 == res.statusCode) {
+
+		} else {
+			throw new Error("Received: " + res.statusCode);
+		}
+}
+
+describe('Routes', function() {
+	it('should return a 200', function(done){
+		request(app)
+			.get('/')
+			.expect(200, done);
+	});
+
+	it('should return a 303 or 200 for /blog path', function(done) {
+		request(app)
+			.get('/blog')
+			.expect(checkForMultipleGhostStatusCodes)
+			.end(done);
+	});
+})
