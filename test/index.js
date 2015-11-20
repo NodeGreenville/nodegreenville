cf4324836a4ffa56b7cadcb38e10ba718c59143e
Javis Sullivan <javissullivan@gmail.com>
2015-11-20 12:07:32 -0500

    fixed test for events endpoint

--- test/index.js
+++ test/index.js
@@ -5,6 +5,23 @@ var hbs = require('express-hbs');
 var app = require('../app');
 var events = require('../routes/events')
 
+function blogStatusCodes(res) {
+	if (303 == res.statusCode || 200 == res.statusCode) {
+
+	} else {
+		throw new Error('Received: ' + res.statusCode);
+	}
+}
+
+function eventStatusCodes(res) {
+	code = res.statusCode;
+	if (200 == code || 304 == code) {
+
+	} else {
+		throw new Error('Received: ' + code);
+	}
+}
+
 describe('Routes', function() {
 	it('should respond with 200 from home', function(done){
 		request(app)
@@ -13,15 +30,18 @@ describe('Routes', function() {
 	});
 
 	it('should respond with 200 from events', function(done) {
+		this.timeout(5000);
 		request(app)
 			.get('/events')
-			.expect(200, done);
+			.expect(eventStatusCodes)
+			.end(done);
 	});
 
-	it('should respond with 200 from blog', function(done) {
+	it('should respond with 200 or 303 from blog', function(done) {
 		request(app)
-			.get('/blog/')
-			.expect(200, done);
+			.get('/blog')
+			.expect(blogStatusCodes)
+			.end(done);
 	});
 
 	it('should respond with 404 from everything else', function(done) {
