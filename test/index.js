f105e1fb21f888b1d39579258b21dd9fb8d78105
Javis Sullivan <javissullivan@gmail.com>
2015-11-30 14:50:56 -0500

    fixes issue#20

--- test/index.js
+++ test/index.js
@@ -1,15 +1,23 @@
 var chai = require('chai');
+var assert = chai.assert;
+var fs = require('fs');
 var expect = chai.expect;
-var request = require('supertest');
+var request = require('supertest-as-promised');
 var hbs = require('express-hbs');
 var app = require('../app');
-var events = require('../routes/events')
+var events = require('../routes/events');
 
 describe('Routes', function() {
+	var htmlHead;
 	it('should respond with 200 from home', function(done){
+		this.timeout(5000);
 		request(app)
 			.get('/')
-			.expect(200, done);
+			.expect(200, function(err, res) {
+				if (err) done(err);
+				htmlHead = res.text; // saving for round-trip comparison below
+				done();
+			});
 	});
 
 	it('should respond with 200 from events', function(done) {
@@ -27,8 +35,35 @@ describe('Routes', function() {
 		this.timeout(5000);
 		request(app)
 			.get('/blog/') // this returns 303 sans the last forward slash
-			.expect(200, done);
+			.expect(200, done)
 	});
+
+	it('should contain correct head html when navigating back to home from blog',
+			function(done) {
+				var agent = request.agent(app);
+				agent
+					.get('/')
+					.expect(200, function(err, res) {
+						if (err) done(err);
+					});
+			  agent.get('/blog/')
+					.expect(200, function(err, res) {
+						if (err) done(err);
+					});
+				agent.get('/') // navigate back to home
+					.expect(200, function(err, res) {
+
+						if (err) done(err);
+						$ = require('cheerio').load(htmlHead);  // original html <head>
+						$$ = require('cheerio').load(res.text); // round trip html <head>
+						/**
+						 * This should be true if the round trip <head> tag has just as many
+						 * children as the original <head> tag. This will fail otherwise.
+						 */
+						assert.equal($$('head').children().length, $('head').children().length);
+						done();
+					});
+				});
 
 	it('should respond with 404 from everything else', function(done) {
 		request(app)
