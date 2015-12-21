01e0262fce981867240c835e684e3cef2221a0a3
Javis Sullivan <javissullivan@gmail.com>
2015-12-21 13:05:06 -0500

    Removed ghost blog infection

--- test/index.js
+++ test/index.js
@@ -8,16 +8,12 @@ var app = require('../app');
 var events = require('../routes/events');
 
 describe('Routes', function() {
-	var htmlHead;
+
 	it('should respond with 200 from home', function(done){
 		this.timeout(5000);
 		request(app)
 			.get('/')
-			.expect(200, function(err, res) {
-				if (err) done(err);
-				htmlHead = res.text; // saving for round-trip comparison below
-				done();
-			});
+			.expect(200, done);
 	});
 
 	it('should respond with 200 from events', function(done) {
@@ -30,40 +26,6 @@ describe('Routes', function() {
 			.get('/events')
 			.expect(200, done);
 	});
-
-	it('should respond with 200 blog', function(done) {
-		this.timeout(5000);
-		request(app)
-			.get('/blog/') // this returns 303 sans the last forward slash
-			.expect(200, done)
-	});
-
-	it('should contain correct head html when navigating back to home from blog',
-			function(done) {
-				var agent = request.agent(app);
-				agent
-					.get('/')
-					.expect(200, function(err, res) {
-						if (err) done(err);
-					});
-			  agent.get('/blog/')
-					.expect(200, function(err, res) {
-						if (err) done(err);
-					});
-				agent.get('/') // navigate back to home
-					.expect(200, function(err, res) {
-
-						if (err) done(err);
-						$ = require('cheerio').load(htmlHead);  // original html <head>
-						$$ = require('cheerio').load(res.text); // round trip html <head>
-						/**
-						 * This should be true if the round trip <head> tag has just as many
-						 * children as the original <head> tag. This will fail otherwise.
-						 */
-						assert.equal($$('head').children().length, $('head').children().length);
-						done();
-					});
-				});
 
 	it('should respond with 404 from everything else', function(done) {
 		request(app)
