3920b3b2e063af56f7520afaff93df404ee5cd90
Javis Sullivan <javissullivan@gmail.com>
2016-01-21 13:14:14 -0500

    Logging in Group Members only works

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var GitHubStrategy = require('passport-github2').Strategy;
+var github = require('octonode');
 
 // Authentication setup
 module.exports = function (passport) {
@@ -11,7 +12,23 @@ module.exports = function (passport) {
 			callbackURL: 'http://localhost:3000/auth/github/callback'
 		},
 		function (accessToken, refreshToken, profile, done) {
-			return done(null, profile);
+
+			// check if user is a member of the group
+			var client = github.client(accessToken);
+			client.get('/user/orgs', {}, function (err, status, body, headers) {
+				var orgIDs = body.map(function (obj) {
+					return obj.id;
+				});
+
+				var NODEGREENVILLE_ID = parseInt(process.env.NODEGREENVILLE_ID);
+				var member = orgIDs.indexOf(NODEGREENVILLE_ID) >= 0;
+				if (member) {
+					// User is apart of the NodeGreenville Group log them in
+					return done(null, profile);
+				} else {
+					return done(null, false);
+				}
+			});
 		}
 	));
 
@@ -23,10 +40,10 @@ module.exports = function (passport) {
 		done(null, obj);
 	});
 
-	router.get('/github', passport.authenticate('github', { scope: ['user:email']}));
+	router.get('/github', passport.authenticate('github', { scope: ['user:email', 'read:org']}));
 	router.get('/github/callback', passport.authenticate('github', {
 	  failureRedirect: '/',
-		failureFlash: 'Login failed'
+		failureFlash: true
 	}),
 	function (req, res) {
 	  res.redirect('/');
