d720ea8f2081d974c9061b42479d6a1c17a586b1
Brent Westmoreland <brent@westmoreland.us>
2016-02-09 20:09:45 -0500

    Updates the stack trace behavior for staging environment

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get('env') === 'development' || 'staging') {
 	app.use(function(err, req, res, next) {
 		res.status(err.status || 500);
 		res.render('error', {
