b9a99774ec92bb4bbd5e7b78711af79c31c1b5fe
Javis Sullivan <javissullivan@gmail.com>
2016-03-21 12:12:45 -0400

    play nice with travis

--- bin/run-nightwatch.js
+++ bin/run-nightwatch.js
@@ -3,10 +3,11 @@ const spawn = require('child_process').spawn;
 var child;
 
 if(os.platform() === 'win32') {
+  var env = (process.env.REST_KEY)? 'BDD_TRAVIS,TDD': 'BDD,TDD';
   child = spawn('node', [
     './nightwatch.js',
     '--config', 'nightwatch.conf.js',
-    '--env', 'BDD,TDD'
+    '--env', env
     ],
     {stdio: 'inherit'}  // pipe nightwatch messages to parent stdio
   );
@@ -17,10 +18,11 @@ if(os.platform() === 'win32') {
     ]
   );
   child.on('close', function() {
+    var env = (process.env.REST_KEY)? 'BDD_TRAVIS,TDD': 'BDD,TDD';
     var child1 = spawn('node', [
       'nightwatch',
       '--config', 'nightwatch.conf.js',
-      '--env', 'BDD,TDD'
+      '--env', env
       ],
       {stdio: 'inherit'}  // Same here. Pipe nightwatch output to parent process
     );
